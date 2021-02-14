e3fa1d6770335a6702fd561a898cfc8d91b57eed
Thomas-Guibert <thomas.guibert0930@gmail.com>
2021-02-14 17:16:08 +0100

    Ajoute fonction de rotation

--- periodic.c
+++ periodic.c
@@ -5,6 +5,7 @@
 #include "ecrobot_emul.c"
 #include "ecrobot_emul.h"
 #include "tpl_os.h"
+
 #define _XOPEN_SOURCE 500
 
 int main(void)
@@ -70,17 +71,43 @@ TASK(Navigation){
   if(err == E_OK)
   {
     if(periode%10 == 0){
-      printf("période : %d\r\n",periode);
+      //Stop
+      ecrobot_set_motor_speed(NXT_PORT_A,0);
+      //tourne
+      int randAng = rand()%360;
+      int randDirect = rand()%2;
+      if(randDirect == 0){
+        nxt_motor_set_count(NXT_PORT_A,-(randAng));
+        printf("[NAVIGATION] période : %d | a tourner a Gauche a %d\r\n",periode,randAng);
+      }else{
+        nxt_motor_set_count(NXT_PORT_A,(randAng));
+        printf("[NAVIGATION] période : %d | a tourner a Droite a %d\r\n",periode,randAng);
+      } 
+      //Avance
+      ecrobot_set_motor_speed(NXT_PORT_A,100);
+      
     }
     else {
       if(val_distance == 0){
         printf("[NAVIGATION] Marche arrière\r\n");
         val_distance += 10;
+        //Marche arriere pour les deux moteur
+        ecrobot_set_motor_speed(NXT_PORT_A,-100);
       }
       if(val_distance < 50){
         val_distance = ecrobot_get_sonar_sensor(NXT_PORT_S1);
         printf("[NAVIGATION] On tourne, nouvelle distance : %d\r\n", val_distance);
-        
+        //Stop
+        ecrobot_set_motor_speed(NXT_PORT_A,0);
+        //Tourne de 90° droit ou gauche
+        int randDirect = rand()%2;
+        if(randDirect == 0){
+          nxt_motor_set_count(NXT_PORT_A,-(90));
+        }else{
+          nxt_motor_set_count(NXT_PORT_A,(90));
+        }
+        //Avance
+        ecrobot_set_motor_speed(NXT_PORT_A,100);
       }
     }
     err = ReleaseResource(resource_distance);
@@ -97,3 +124,74 @@ TASK(stop)
   ShutdownOS(E_OK);
   TerminateTask();
 }
+
+/*
+int main(void)
+{
+    StartOS(OSDEFAULTAPPMODE);
+    ecrobot_init_sonar_sensor(NXT_PORT_S1);
+    return 0;
+}
+
+int distance = 100;
+
+DeclareAlarm(alarmForContact);
+DeclareAlarm(alarmForDistance);
+DeclareAlarm(alarmForNavigation);
+
+TASK(Detection_contact)
+{
+StatusType err;
+err = GetResource(&distance);
+if(err == E_OK){
+
+  int touch = ecrobot_get_touch_sensor(NXT_PORT_S2);
+  //touch 0 = False | touch 255 = True
+  if(touch == 1 && distance != 0){
+    distance = 0;
+    printf("[CONTACT CAPTOR]      Le touch est a : %d. La distance change et passe a %d \r\n", touch ,distance);
+  }else{
+    printf("[CONTACT CAPTOR]      Le touch est a : %d. La distance reste de : %d \r\n", touch ,distance);
+  }
+}
+  TerminateTask();
+}
+
+
+TASK(Detection_distance)
+{
+  distance = ecrobot_get_sonar_sensor(NXT_PORT_S1)%255;
+  printf("[DISTANCE CAPTOR]     La distance est maintenant de  : %d \r\n",distance);
+  TerminateTask();
+}
+
+
+TASK(Navigation){
+  static int periode = 0;
+  if(periode ==10){
+    periode =0;
+    printf("[NAVIGATION CAPTOR]   changement de direction \r\n");
+  }
+  if(distance == 0){
+    printf("[NAVIGATION CAPTOR]   Marche arriere \r\n");
+    distance += 10;
+  }
+  if(distance < 50){
+    distance = ecrobot_get_sonar_sensor(NXT_PORT_S1)%255;
+    printf("[NAVIGATION CAPTOR]   On tourne, nouvelle distance %d\r\n",distance);
+    
+  }
+  periode++;
+  TerminateTask();
+}
+
+
+TASK(stop)
+{
+  ecrobot_term_sonar_sensor(NXT_PORT_S1);
+  CancelAlarm(Detection_contact);
+  printf("Shutdown\r\n");
+  ShutdownOS(E_OK);
+  TerminateTask();
+}
+*/
